0657da0a55c7d52c317210f52dae55fd4ce8a0ba
gallagherpat <gallagherpatrick21@gmail.com>
2023-09-12 16:30:15 -0400

    Add item into transaction

--- app/api/dummy/get/recommendations/route.ts
+++ app/api/dummy/get/recommendations/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
     const JSON = {
     "data":[{
-                "itemID": 64321,
+                "itemID": 80001,
                 "price": 35.99,
                 "receiptText": "Whiskey",
                 "registrationNumber": 543210000,
--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -2,6 +2,25 @@ import Image from "next/image";
 //@ts-ignore
 function Modal(props){
     const item = props.item;
+    const sItemID = item.itemID;
+    let addItemButton = (<button className="bg-[#333] ml-2 mb-4 text-sm py-1 px-2 rounded-full text-white" onClick={()=>{
+      let oAppEnablementPosInstance = new comGkSoftwareGkrAppEnablementApi.Pos();
+      console.log("click")
+      const sendRegisterItem = function(item: number){
+        console.log(item);
+        let registerLineItemRequest = JSON.stringify({
+            "itemID": item
+        });
+        oAppEnablementPosInstance.registerLineItem(registerDataOk, registerDataFailed, registerLineItemRequest);
+        }
+        function registerDataOk() {
+          console.log("Succesfully registered", "success");
+        }
+        function registerDataFailed() {
+          console.error("Data registration failed");
+        }
+        sendRegisterItem(sItemID);
+    }}>Add to Cart</button>)
     return (<div className="bg-white rounded-md min-h-fit z-10 p-2 absolute inset-6 top-32 mix-blend-normal">
         <button onClick={props.setModal}>X</button>
         <Image
@@ -15,7 +34,7 @@ function Modal(props){
         <p  className="text-sm font-thin ml-2">afterpay</p>
         <p  className="text-sm ml-2">This is a tasty red wine</p>
         <div className="bg-[#333] h-[1px] mx-2 my-3 opacity-20"></div>
-        <button  className="bg-[#333] ml-2 mb-4 text-sm py-1 px-2 rounded-full text-white">Add to Cart</button>
+        {addItemButton}
     </div>
     );
 }
